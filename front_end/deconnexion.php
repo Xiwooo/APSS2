72a3c7896133662b03306e095b94287bdf47ff3f
RousseauErwan <rou.erwan@gmail.com>
2022-03-18 15:29:01 +0100

    ijejokd

--- front_end/deconnexion.php
+++ front_end/deconnexion.php
@@ -3,7 +3,7 @@
  * * NR le 24/12/2020
  *   ce fichier permet de réinitialiserles variables de session et 
  *    déconnecté l'utilisateur
- *    L'affichage propose de nouyveau les deux types d'utilisateur
+ *    L'affichage propose de nouyveau les deux types d'utilisateurhjkjl&érefdhyuezrjkfduirhgjk
  **/ 
  session_start();
  session_destroy();
