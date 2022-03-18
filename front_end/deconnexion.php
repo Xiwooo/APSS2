54c42a08dec4f216dc08d1f6f5c3204316b76c6f
RousseauErwan <rou.erwan@gmail.com>
2022-03-18 16:03:09 +0100

    zeze

--- front_end/deconnexion.php
+++ front_end/deconnexion.php
@@ -3,7 +3,7 @@
  * * NR le 24/12/2020
  *   ce fichier sddsdspermet de réinitialiserles variables de session et 
  *    déconnecté l'utisdlisateurzeefzfzezefzer
- *    L'affichage propose de nouyveau les deux types d'utilisateur
+ *    L'affichage propose de nouyveau les deuxd types d'utilisateur
  **/ 
  session_start();
  session_destroy();
