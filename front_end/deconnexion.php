2b2b8001f7c4ab9ed76e276083f279f1f890b8d9
RousseauErwan <rou.erwan@gmail.com>
2022-03-18 15:54:46 +0100

    ezd

--- front_end/deconnexion.php
+++ front_end/deconnexion.php
@@ -2,7 +2,7 @@
 /**
  * * NR le 24/12/2020
  *   ce fichier permet de réinitialiserles variables de session et 
- *    déconnecté l'utilisateur
+ *    déconnecté l'utisdlisateur
  *    L'affichage propose de nouyveau les deux types d'utilisateur
  **/ 
  session_start();
