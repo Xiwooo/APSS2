f0a8164619e6f6a3f210030a2caba3d8c06459c1
RousseauErwan <rou.erwan@gmail.com>
2022-03-18 15:59:56 +0100

    dc

--- front_end/deconnexion.php
+++ front_end/deconnexion.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * * NR le 24/12/2020
- *   ce fichier permet de réinitialiserles variables de session et 
+ *   ce fichier sddsdspermet de réinitialiserles variables de session et 
  *    déconnecté l'utisdlisateurzeefzfzezef
  *    L'affichage propose de nouyveau les deux types d'utilisateur
  **/ 
